6a0ceaa2896d001a411634be95b7d54c679b08b9
Oliver Lambert Rouillard <parties01smacks@icloud.com>
2024-08-07 21:15:56 -0400

    basic card no logic

---
+++ FlashCard_JS/src/components/cards.vue
@@ -0,0 +1,13 @@
+<script setup>
+	import card from '@/components/card.vue'
+</script>
+
+<template>
+	<div class="m-5">
+		<card/>
+		<button type="button" class="btn btn-success">Success</button>
+		<button type="button" class="btn btn-danger">Success</button>
+	</div>
+</template>
+
+
