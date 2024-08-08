182987d76d0e3d2f0f1a1e8c2deaa9fe714305c7
Oliver Lambert Rouillard <parties01smacks@icloud.com>
2024-08-07 21:15:56 -0400

    basic card no logic

---
+++ FlashCard_JS/src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+	import cards from '@/components/cards.vue'
+</script>
+
+<template>
+	<cards/>
+</template>
---
+++ FlashCard_JS/src/components/card.vue
@@ -0,0 +1,37 @@
+<script setup>
+  import {ref, reactive} from 'vue';
+
+  const word = reactive({
+    word: "placeholder",
+    answer: "answer",
+    last: "2024/01/01"
+  })
+
+</script>
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 card-wrapper"> 
+        <div class="card custom-card">
+          <img src="https://picsum.photos/200/200?random=1" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">{{word.word}}</h5>
+            <p class="card-text">{{word.answer}}</p>
+            <p class="card-text"><small class="text-muted">Last try: {{word.last}}</small></p>
+          </div>  
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+  .custom-card {
+  width: 20%;
+  min-width: 200px;
+  }
+  .card-wrapper {
+    display: flex;
+    justify-content: center;
+  }
+</style>
