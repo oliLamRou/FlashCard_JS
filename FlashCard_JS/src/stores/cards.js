20b90e202c984ff8588a11509c63567cc4ae1746
Olivier Lambert Rouillard <parties01smacks@icloud.com>
2024-08-21 11:09:02 -0400

    basic logic done.

--- FlashCard_JS/src/stores/cards.js
+++ FlashCard_JS/src/stores/cards.js
@@ -3,32 +3,43 @@ import axios from 'axios'
 
 export const useCards = defineStore('pairForm',{
   state:() => ({
-    cards: [],
-    category: null
+    deck: [],
+    category: null,
+    current: 0
   }),
   getters: {
     categories() {
-      return new Set(this.cards.map(item => item.category))
-    }
+      return new Set(this.deck.map(item => item.category))
+    },
+    cards() {
+      return this.deck.filter(card => card.category === this.category)
+    },
+    card() {
+      return this.cards[this.current]
+    },
   },
   actions:{
     async load() {
       try {
         const responce = await axios.get('http://localhost:5003/get_words')
-        this.cards = responce.data
+        this.deck = responce.data
       }
       catch(error) {
         console.log(error)
       }
     },
+    draw_card() {
+      this.current =  Math.floor(Math.random() * this.cards.length)
+    },    
     update_category(newCategory) {
       this.category = newCategory
+      this.draw_card()
     },
     bad_answer(newCategory) {
-      // this.category = newCategory
+      this.draw_card()
     },
     good_answer(newCategory) {
-      // this.category = newCategory
+      this.draw_card()
     }
   },
 })
