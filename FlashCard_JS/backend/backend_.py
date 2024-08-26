e350c216107d587c44fd4d83578644661bbe00e4
Olivier Lambert Rouillard <parties01smacks@icloud.com>
2024-08-26 17:26:44 -0400

    ip flask restful and sql

---
+++ FlashCard_JS/backend/backend_.py
@@ -0,0 +1,54 @@
+import ast
+from pathlib import Path
+import requests
+
+import pandas as pd 
+from flask import Flask, request
+from flask_cors import CORS
+
+from flask_restful import Api, Resource
+
+PROJECT_ROOT = Path(__file__).parent.parent.parent
+DB = (PROJECT_ROOT / 'data' / 'words.csv').resolve()
+
+app = Flask(__name__)
+CORS(app)
+
+def deck():
+	return pd.read_csv(DB) 
+
+@app.route('/load_deck', methods=['GET'])
+def load_deck():
+	return deck().to_json(orient='records')
+
+@app.route('/generate', methods=['POST'])
+def generate():
+	userInput = request.json.get('userInput')
+	URL = 'http://localhost:11434/api/generate'
+	n = userInput.get('amount')
+	topic = userInput.get('topic')
+	languageA = userInput.get('languageA')
+	languageB = userInput.get('languageB')
+	question = f"Please provide {n} words related to {topic} in {languageA} and {languageB}. "
+	request_format = f"Format: [{{'{languageA}': value, '{languageB}': value}}...]. "
+	details = "Provide only the list"
+
+	d = {
+	  "model": "gemma2",
+	  "prompt": question + request_format + details,
+	  "stream": False
+	}
+
+	data = requests.post(URL, json=d)
+	response = data.json().get('response')
+	new_cards = pd.DataFrame(ast.literal_eval(response))
+	new_cards = new_cards.rename(columns={languageA: 'question', languageB: 'answer'})
+	new_cards[['topic', 'last', 'image', 'score']] = [topic, '2024-01-01', '', 0]
+	updatedDeck = pd.concat([deck(), new_cards])
+	updatedDeck = updatedDeck[~updatedDeck[['topic', 'question']].duplicated()].reset_index(drop=True)
+	updatedDeck.to_csv(DB, index=False)
+
+	return deck().to_json(orient='records')
+
+if __name__ == '__main__':
+	app.run(debug=True,  port='5003')
